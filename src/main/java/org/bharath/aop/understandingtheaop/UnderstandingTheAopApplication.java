ebf6e0b80fdf015ca506d8331e0b83a94cf0584a
Bharath Kumar <bharathk494@gmail.com>
2020-05-11 07:54:44 +0530

    Initial Commit

---
+++ src/main/java/org/bharath/aop/understandingtheaop/UnderstandingTheAopApplication.java
@@ -0,0 +1,13 @@
+package org.bharath.aop.understandingtheaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingTheAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderstandingTheAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bharath/aop/understandingtheaop/UnderstandingTheAopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bharath.aop.understandingtheaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingTheAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
